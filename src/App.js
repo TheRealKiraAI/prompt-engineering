db964637556d0618470df258e7e81d18a1a16c37
KiraCorbett <kiracorbett00@gmail.com>
2024-05-16 13:59:53 -0700

    step 1: setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ChordIdentifier from "./ChordIdentifier";
+
+function App() {
+  return (
+    <div className="App">
+      <ChordIdentifier />
+    </div>
+  );
+}
+
+export default App;
