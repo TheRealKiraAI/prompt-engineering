5d9feb2a51196c79ee6642cfeaa050e4e92bb870
KiraCorbett <kiracorbett00@gmail.com>
2024-06-26 14:45:43 -0700

    finished exercise 1

--- 01-building-better-prompts/src/app/page.js
+++ 01-building-better-prompts/src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Chordy Music AI App 🎹</h1>
+      <h1>AI Assistant App 💬</h1>
       <Chat />
     </main>
   );
