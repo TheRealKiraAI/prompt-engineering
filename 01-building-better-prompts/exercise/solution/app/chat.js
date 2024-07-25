26cc895ada04a257981754473923977cef625a30
KiraCorbett <kiracorbett00@gmail.com>
2024-07-25 12:50:14 -0700

    01-solution npm migration

--- 01-building-better-prompts/exercise/solution/app/chat.js
+++ 01-building-better-prompts/exercise/solution/app/chat.js
@@ -6,7 +6,7 @@ export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
-    <div>
+    <div style={{ border: "1px solid #ccc", padding: "10px", borderRadius: "5px" }}>
       {messages.map((m, index) => (
         <div key={index}>
           {m.role === "user" ? "User: " : "AI: "}
@@ -15,7 +15,17 @@ export default function Chat() {
       ))}
 
       <form onSubmit={handleSubmit}>
-        <input value={input} placeholder="Say something..." onChange={handleInputChange} />
+        <input
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+          style={{
+            padding: "5px",
+            borderRadius: "3px",
+            border: "1px solid #aaa",
+            width: "100%",
+          }}
+        />
       </form>
     </div>
   );
