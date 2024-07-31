9804e15ee8004fe8ab0af5e4199b132012789ff5
KiraCorbett <kiracorbett00@gmail.com>
2024-07-31 14:42:18 -0700

    02 temp fix for ml5

--- 02-pretrained-models/my-app/app/page.js
+++ 02-pretrained-models/my-app/app/page.js
@@ -1,13 +1,13 @@
-"use client";
-
+import Image from "next/image";
 import Notes from "./notes";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Chordy Music AI App 🎹</h1>
-      <Notes />
+      <div className={styles.description}>
+        <Notes />
+      </div>
     </main>
   );
 }
